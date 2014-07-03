51bc3fb120d2fc8efeead2040f6e93e13df83ea9
Seth Law <seth@nvisium.com>
2014-07-03 15:12:17 -0600

    Addition of Help Screen.

---
+++ Swift.nV/NVHelpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NVHelpViewController.swift
+//  Swift.nV
+//
+//  Created by Seth Law on 7/3/14.
+//  Copyright (c) 2014 nVisium. All rights reserved.
+//
+
+import UIKit
+
+class NVHelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close(sender : AnyObject) {
+        self.dismissModalViewControllerAnimated(true)
+    }
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
