1ad368050ceb09070ecff420ca1f92a52b6012e3
cktricky <ken.johnson@nvisiumsecurity.com>
2014-08-19 18:58:12 -0400

    looking good so far

--- api/controller/tutorials_controller.rb
+++ api/controller/tutorials_controller.rb
@@ -1,6 +1,12 @@
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
-require 'erb'
+helpers do
+
+  def partial(template)
+    erb template, :layout => false
+  end
+
+end
 
 get '/tutorial' do
   erb :index
